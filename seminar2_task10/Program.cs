1e68e18f5d23f5268f22589f10e916855b34606d
Nina Konopleva <orangemise@gmail.com>
2023-02-01 01:24:33 +0300

    add task 10, 15,

---
+++ seminar2_task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 99 && n < 1000)
+{
+    int n1 = n % 100;
+    int n2 = n1 / 10;
+    Console.WriteLine(n2);
+} 
+else
+Console.WriteLine("Вы ввели не трехзначное число");
---
+++ seminar2_task15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 5 && n < 8)
+    Console.WriteLine("да");
+else if (n > 0 && n < 6)
+    Console.WriteLine("нет");
+else 
+    Console.WriteLine("Вы ввели число не в пределах от 1 до 7");
