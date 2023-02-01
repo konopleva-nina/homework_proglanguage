0fc2014924631d72fad37407b2bffaee49d20699
Nina Konopleva <orangemise@gmail.com>
2023-02-01 23:50:41 +0300

    task 13

---
+++ seminar2_task13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+Console.WriteLine("третьего числа нет");
+else
+{
+    while (n > 1000) n /= 10;
+    int n1 = n % 10;
+    Console.WriteLine(n1);
+}
+
+
+
+
+
