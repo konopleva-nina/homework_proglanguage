b518f46fe87dacf533b9d97bc3a3af1ae51a2476
Nina Konopleva <orangemise@gmail.com>
2023-02-05 01:36:50 +0300

    task 19,21,23

---
+++ seminar3_task19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n > 100000 || n < 10000)
+{
+    Console.Write("Вы ошиблись!\nВведите пятизначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int d1 = n / 10000, d5 = n % 10;
+int d2 = (n / 1000) % 10, d4 = (n % 100) / 10;
+if (d1 == d5 && d2 == d4)
+Console.WriteLine("да");
+else
+Console.WriteLine("нет");
+
---
+++ seminar3_task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите координату X первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z первой точки: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z второй точки: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double l = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
+Console.WriteLine($"Результат: {l}");
