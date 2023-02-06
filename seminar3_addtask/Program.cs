391a3974f6fa3178ec8e9a69806ed111f2be55a4
Nina Konopleva <orangemise@gmail.com>
2023-02-06 13:00:26 +0300

    add task_vers2

--- seminar3_addtask/Program.cs
+++ seminar3_addtask/Program.cs
@@ -2,24 +2,24 @@
 Console.Write("Введите количество кустов черники: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int s = 0;
-int[] a = new int[n + 2];
+int[] a = new int[n];
 Random rand = new Random();
 for (int i = 0; i < n; i++)
     Console.WriteLine(a[i] = rand.Next(1, 10));
-a[n] = a[0];
-a[n + 1] = a[1];
-for (int i = 1; i < n + 1; i++) // цикл нахождения суммы для каждого i элемента
+for (int i = 1; i < a.Length - 1; i++) 
 {
     int s1 = a[i - 1] + a[i] + a[i + 1];
-    int s2 = a[n] + a[0] + a[1];
-    int s3 = a[n - 1] + a[n] + a[0];
-    if (s1 > s) // условие нахождения максимальной суммы
+
+    if (s1 > s) 
     s = s1;
-    if (s2 > s)
-    s = s2;
-    if (s3 > s)
-    s = s3;
 }
+int s2 = a[a.Length - 1] + a[0] + a[1];
+if (s2 > s)
+s = s2;
+int s3 = a[a.Length - 2] + a[a.Length - 1] + a[0];
+if (s3 > s)
+s = s3;
+
 Console.WriteLine($"максимальная сумма: {s}");
 
 
