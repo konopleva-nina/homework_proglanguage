2daa39cade6642a6fb5751e8d766b9a24404775f
Nina Konopleva <orangemise@gmail.com>
2023-02-09 12:13:16 +0300

    task 29

---
+++ seminar4_task29/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Random rand = new Random();
+for (int i = 0; i < n; i++)
+    array[i] = rand.Next(1, 10);
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.Write("Введите число х из массива: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+for (int i = 0; i < n; i++)
+{
+    if (x == array[i])
+    res = res + 1;
+}
+Console.WriteLine($"Количество вхождений числа х: {res}");
+
+
+
+
+
