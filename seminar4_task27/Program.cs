2a46f5518572774a4efeed86f5dc7396bf6e4dfe
Nina Konopleva <orangemise@gmail.com>
2023-02-09 11:37:13 +0300

    task 27

---
+++ seminar4_task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), sum = 0;
+while (n > 0)
+{
+    int n1 = n % 10;
+    sum = sum + n1;
+    n = n / 10;
+}
+Console.WriteLine($"Сумма цифр в числе: {sum}");
