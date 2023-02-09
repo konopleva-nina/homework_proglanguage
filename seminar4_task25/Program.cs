4d4295bb8c2964aeec92bc7042ad04dafbea002e
Nina Konopleva <orangemise@gmail.com>
2023-02-09 12:11:27 +0300

    task 25

---
+++ seminar4_task25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+for (int i = 1; i <= b; i++)
+{
+    m = m * a;
+}
+Console.WriteLine($"Результат: {m}");
+
+
