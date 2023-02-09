12d6ee57d676fd612030cf1e561b8f7cb349f999
Nina Konopleva <orangemise@gmail.com>
2023-02-09 12:10:11 +0300

    addtask 1

---
+++ seminar4_addtask1/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Random rand = new Random();
+for (int i = 0; i < n; i++)
+    array[i] = rand.Next(1, 10);
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.Write("Введите число сдвига k: ");
+int k = Convert.ToInt32(Console.ReadLine());
+int[] b = new int[n]; // вводим дополнительный массив
+for (int j = n - 1; j >= 0; j--)
+{
+    if (k > 0)
+    {
+    if (j + k >= n) 
+    {
+        b[j + k - n] = array[j];
+    }
+    else
+    {
+        b[j + k] = array[j];
+    }
+    }
+    else
+    {
+    if (j + k < 0)
+    {
+        b[j + k + n] = array[j];
+    }
+    else
+    {
+        b[j + k] = array[j];
+    }
+    }
+
+} 
+Console.WriteLine($"[{string.Join(", ", b)}]");
