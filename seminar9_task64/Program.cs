d8e91a20dac67eeb07350fc1ffe82eef45139934
Nina Konopleva <orangemise@gmail.com>
2023-02-24 23:59:12 +0300

    task 64, 66, 68

---
+++ seminar9_task64/Program.cs
@@ -0,0 +1,12 @@
+﻿string rec(int n)
+{
+    if (n == 1)
+        return "1";
+    return $"{n}, {rec(n - 1)}";
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
---
+++ seminar9_task66/Program.cs
@@ -0,0 +1,14 @@
+﻿int rec(int m, int n)
+{
+    if (n == m)
+        return m;
+    return n + rec(m, n - 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
